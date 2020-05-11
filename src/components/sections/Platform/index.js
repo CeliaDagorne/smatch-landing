497e243ed094d2e6781c75c4166691b9e53510ec
Célia Dagorne <celia.dagorne@hetic.net>
2020-05-11 12:03:41 +0200

    Add overflow hidden

--- src/components/sections/Platform/index.js
+++ src/components/sections/Platform/index.js
@@ -14,80 +14,77 @@ import window from '../../../assets/images/window.svg'
 const Platform = ({ appeared }) => {
   const titleClass = classNames(styles.title, typography.h2)
   const bodyTextClass = classNames(styles.textBody, typography.eyebrow)
-  const isMobile = window.innerWidth <= 920
 
   const scene = useRef(null)
 
   useEffect(() => {
-    if (!isMobile) {
-      const Engine = Matter.Engine
-      const Render = Matter.Render
-      const World = Matter.World
-      const Bodies = Matter.Bodies
-      const Mouse = Matter.Mouse
-      const MouseConstraint = Matter.MouseConstraint
+    const Engine = Matter.Engine
+    const Render = Matter.Render
+    const World = Matter.World
+    const Bodies = Matter.Bodies
+    const Mouse = Matter.Mouse
+    const MouseConstraint = Matter.MouseConstraint
 
 
-      if (appeared) {
-        const engine = Engine.create({
-          // positionIterations: 20
-        })
+    if (appeared) {
+      const engine = Engine.create({
+        // positionIterations: 20
+      })
 
-        const render = Render.create({
-          element: scene.current,
-          engine: engine,
-          options: {
-            background: 'transparent',
-            width: 530,
-            height: 640,
-            wireframes: false
+      const render = Render.create({
+        element: scene.current,
+        engine: engine,
+        options: {
+          background: 'transparent',
+          width: 530,
+          height: 640,
+          wireframes: false
+        }
+      })
+
+      const ball = Bodies.circle(100, 20, 70, { restitution: 0.5, render: {
+        fillStyle: '#F8CA36'
+      }})
+      const rect = Bodies.rectangle(100, 130, 180, 180, { restitution: 0.5, render: {
+        fillStyle: '#4ABC86'
+      }})
+      const triangle = Bodies.polygon(265, 130, 3, 140, { restitution: 0.5, render: {
+        fillStyle: '#FFB4C8'
+      }})
+
+      World.add(engine.world, [
+        // walls
+        Bodies.rectangle(200, 0, 600, 1, { isStatic: true, render: {
+          fillStyle: 'transparent'
+        } }),
+        Bodies.rectangle(0, 640, 1100, 5, { isStatic: true, render: {
+          fillStyle: 'transparent'
+        } }),
+        Bodies.rectangle(530, 300, 10, 670, { isStatic: true, render: {
+          fillStyle: 'transparent'
+        } }),
+        Bodies.rectangle(0, 300, 1, 600, { isStatic: true, render: {
+          fillStyle: 'transparent'
+        } })
+      ])
+
+      // add mouse control
+      const mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
           }
-        })
+        }
+      })
 
-        const ball = Bodies.circle(100, 20, 70, { restitution: 0.5, render: {
-          fillStyle: '#F8CA36'
-        }})
-        const rect = Bodies.rectangle(100, 130, 180, 180, { restitution: 0.5, render: {
-          fillStyle: '#4ABC86'
-        }})
-        const triangle = Bodies.polygon(265, 130, 3, 140, { restitution: 0.5, render: {
-          fillStyle: '#FFB4C8'
-        }})
+      World.add(engine.world, [ball, rect, triangle])
+      World.add(engine.world, mouseConstraint)
 
-        World.add(engine.world, [
-          // walls
-          Bodies.rectangle(200, 0, 600, 1, { isStatic: true, render: {
-            fillStyle: 'transparent'
-          } }),
-          Bodies.rectangle(0, 640, 1100, 5, { isStatic: true, render: {
-            fillStyle: 'transparent'
-          } }),
-          Bodies.rectangle(530, 300, 10, 670, { isStatic: true, render: {
-            fillStyle: 'transparent'
-          } }),
-          Bodies.rectangle(0, 300, 1, 600, { isStatic: true, render: {
-            fillStyle: 'transparent'
-          } })
-        ])
-
-        // add mouse control
-        const mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-          mouse: mouse,
-          constraint: {
-            stiffness: 0.2,
-            render: {
-              visible: false
-            }
-          }
-        })
-
-        World.add(engine.world, [ball, rect, triangle])
-        World.add(engine.world, mouseConstraint)
-
-        Engine.run(engine)
-        Render.run(render)
-      }
+      Engine.run(engine)
+      Render.run(render)
     }
   }, [appeared])
 
